dc991692a429d5d75ea8945b686a81f282aae488
Rafael Arruda <dig.rafaelsilva@opovo.local>
2024-10-28 16:15:12 -0300

    feat: Destaque - variação 2

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// function addDividingLine() {
+//     const emphasis = document.querySelector('.c-emphasis');
+
+//     if (emphasis?.classList.contains('var-two')) {
+//         const boxMoreNews = document.querySelector('.c-box-more-news');
+//         const news = document.querySelectorAll('.c-box-more-news .c-box-news');
+    
+//         if (boxMoreNews) {
+//             boxMoreNews.classList.add('dividing-line');
+//         }
+    
+//         // Inserindo linha divisória nas três últimas matérias
+//         news.forEach((item, index) => {
+//             if (index > 2) {
+//                 item.classList.add('dividing-line');
+//             }
+//         });
+//     }
+// }
+
+// addDividingLine();
